ad60f0a9d0c959f4c2d610b7cbb13ba008cbd4c4
samarth088 <skshah987654@gmail.com>
2025-06-10 00:24:47 +0530

    Update Post.jsx

--- frontend/components/Post.jsx
+++ frontend/components/Post.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+import CommentList from "./CommentList";
 
+export default function Post({ post, currentUser }) {
+  // ... existing post display code ...
+  return (
+    <div>
+      {/* Your post rendering here */}
+      <h3>{post.content}</h3>
+      {/* Comments */}
+      <CommentList postId={post._id} currentUser={currentUser} />
+    </div>
+  );
+}
