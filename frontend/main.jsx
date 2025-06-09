4bfc6092ed213329f05fa562440027d979739e1a
samarth088 <skshah987654@gmail.com>
2025-06-09 21:27:04 +0530

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+function App() {
+  return <h1>Welcome to VibeSphere!</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
