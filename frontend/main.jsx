491e0741f258b875efeac579ad3c26113361cc2b
samarth088 <skshah987654@gmail.com>
2025-06-09 21:56:03 +0530

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Hello from './components/Hello';
+import AuthForm from './components/AuthForm';
 
 function App() {
   return (
     <div>
       <h1>Welcome to VibeSphere!</h1>
       <Hello />
+      <AuthForm />
     </div>
   );
 }
