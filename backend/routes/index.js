e2a843323c56cf3a3d62bf8f32814f1c190be08b
samarth088 <skshah987654@gmail.com>
2025-06-09 22:44:04 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const authRoutes = require('./auth');
-const userRoutes = require('./user'); // NEW
+const userRoutes = require('./user');
+const postRoutes = require('./post'); // NEW
 
 router.get('/hello', (req, res) => {
   res.json({ message: "Hello from VibeSphere API!" });
 });
 
 router.use('/auth', authRoutes);
-router.use('/user', userRoutes); // NEW
+router.use('/user', userRoutes);
+router.use('/posts', postRoutes); // NEW
 
 module.exports = router;
