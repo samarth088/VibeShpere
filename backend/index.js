f1114e9b4b8b9ff0cab96377d2ad294d10958b59
samarth088 <skshah987654@gmail.com>
2025-06-09 21:35:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const routes = require('./routes');
+
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', routes); // All routes prefixed with /api
 
-// Test route
 app.get('/', (req, res) => {
   res.send('VibeSphere Backend is running!');
 });
