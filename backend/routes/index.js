1cdeb0aaedf3e7121212485c192374a57ce18267
samarth088 <skshah987654@gmail.com>
2025-06-09 21:33:45 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
 
+// Sample API route
+router.get('/hello', (req, res) => {
+  res.json({ message: "Hello from VibeSphere API!" });
+});
+
+module.exports = router;
