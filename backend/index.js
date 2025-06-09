fa73c8bf8c3cf3916445d06dd4bd919ab9a2e493
samarth088 <skshah987654@gmail.com>
2025-06-09 22:07:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./db');
 
 const app = express();
 const routes = require('./routes');
@@ -9,12 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', routes); // All routes prefixed with /api
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
   res.send('VibeSphere Backend is running!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
 });
