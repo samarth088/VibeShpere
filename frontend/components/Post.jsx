da598ab40b3b7f725f70b2ff5024a22b9cfe6eb2
samarth088 <skshah987654@gmail.com>
2025-06-10 10:07:53 +0530

    Update Post.jsx

--- frontend/components/Post.jsx
+++ frontend/components/Post.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import LikeButton from "./LikeButton";
 import CommentList from "./CommentList";
 
 export default function Post({ post, currentUser }) {
-  // ... existing post display code ...
+  const [likesCount, setLikesCount] = useState(post.likes.length);
+  const [likesArr, setLikesArr] = useState(post.likes);
+
+  const handleLike = (newLikesCount, action) => {
+    setLikesCount(newLikesCount);
+    if (action === "like") setLikesArr([...likesArr, currentUser]);
+    else setLikesArr(likesArr.filter((id) => id !== currentUser));
+  };
+
   return (
     <div>
-      {/* Your post rendering here */}
       <h3>{post.content}</h3>
-      {/* Comments */}
+      <LikeButton post={{ ...post, likes: likesArr }} currentUser={currentUser} onLike={handleLike} />
+      <span>{likesCount} {likesCount === 1 ? "like" : "likes"}</span>
       <CommentList postId={post._id} currentUser={currentUser} />
     </div>
   );
