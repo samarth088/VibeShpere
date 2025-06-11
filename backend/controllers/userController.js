ba0b83bf17bd35d00d38707da2404f650a5c65f6
samarth088 <skshah987654@gmail.com>
2025-06-11 15:27:24 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,19 @@
+const User = require('../models/User');
 
+// Example controller for register
+exports.registerUser = async (req, res) => {
+  try {
+    // Logic to create user, hash password, etc.
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Example controller for login
+exports.loginUser = async (req, res) => {
+  try {
+    // Logic to authenticate user, issue token, etc.
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
