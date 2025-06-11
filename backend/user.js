e491e56ea2b149e426f4ec94f079e47376c81235
samarth088 <skshah987654@gmail.com>
2025-06-11 15:54:25 +0530

    Update user.js

--- backend/user.js
+++ backend/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
