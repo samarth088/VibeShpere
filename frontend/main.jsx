8608417906ebcd456b71526ad0689db27daf8942
samarth088 <skshah987654@gmail.com>
2025-06-12 16:44:55 +0530

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,26 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import AuthForm from "./components/AuthForm";
-import Feed from "./components/Feed";
-import CreatePost from "./components/CreatePost";
-
-function App() {
-  const [postRefresh, setPostRefresh] = useState(0);
-
-  const token = localStorage.getItem("token");
-
-  return (
-    <div>
-      <h1>Welcome to VibeSphere!</h1>
-      {!token && <AuthForm />}
-      {token && (
-        <>
-          <CreatePost onPostCreated={() => setPostRefresh(r => r + 1)} />
-          <Feed key={postRefresh} />
-        </>
-      )}
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
