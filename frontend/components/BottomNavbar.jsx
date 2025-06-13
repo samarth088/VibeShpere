6fa1db56b97c0b8aa7213f97f5f803ee1dedc128
samarth088 <skshah987654@gmail.com>
2025-06-13 15:28:02 +0530

    Create BottomNavbar.jsx

---
+++ frontend/components/BottomNavbar.jsx
@@ -0,0 +1,12 @@
+import BottomNavbar from "./components/BottomNavbar";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        {/* your routes here */}
+      </Routes>
+      <BottomNavbar /> {/* Always visible at bottom */}
+    </>
+  );
+}
