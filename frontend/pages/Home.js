b7bb2536e9ab09ad93e250131915bf3227358909
samarth088 <skshah987654@gmail.com>
2025-06-13 15:12:34 +0530

    Create Home.js

---
+++ frontend/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-semibold">Home Feed</h2>
+      {/* Map post cards here */}
+    </div>
+  );
+}
