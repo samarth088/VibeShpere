f9549302a8390a2e7ee75730cfd5c42923627b8d
samarth088 <skshah987654@gmail.com>
2025-06-15 21:29:21 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypts');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 const auth = require('../middleware/auth');
