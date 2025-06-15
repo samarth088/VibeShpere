f6ddeb84b61fb58a3be08b669637591801c8358c
samarth088 <skshah987654@gmail.com>
2025-06-15 22:36:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['lucide-react']
+  },
+  resolve: {
+    alias: {
+      'lucide-react': 'lucide-react'
+    }
+  }
 });
